1d5346867bb620b1456c8ae28e8216cc9ee794d3
Harry LB <harry.lingardbright@gmail.com>
2020-07-13 15:17:24 +0100

    completed chapter 6, added a balance limit and a test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,15 @@
 class Oystercard
 
   attr_accessor :balance
-
+  
+  BALANCE_LIMIT = 90
   def initialize(balance = 0)
     @balance = balance
+  end
+
+  def top_up(amount)
+    fail "Balance cannot exceed #{BALANCE_LIMIT}" if @balance + amount > BALANCE_LIMIT
+    @balance += amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,15 @@ describe Oystercard do
   it 'Adds balance of zero to a new card' do
     expect(subject.balance).to eq(0)
   end
+
+  describe '#top_up(amount)' do
+    it 'increases the balance by amount' do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10 
+    end
+    it 'throw an error if the new balance is above limit' do 
+      subject.top_up(Oystercard::BALANCE_LIMIT)
+      expect { subject.top_up(1) }.to raise_error "Balance cannot exceed #{Oystercard::BALANCE_LIMIT}"
+    end
+  end
 end

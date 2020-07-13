c02332fbc9916c01ae742a5f33297438354c96f5
Paul Humphreys <paulhumphreys81@aol.co.uk>
2020-07-13 15:50:17 +0100

    Chapter 8: Touch in/out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,41 @@
 class Oystercard
 
   attr_accessor :balance
-  
+
   BALANCE_LIMIT = 90
   def initialize(balance = 0)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(amount)
     fail "Balance cannot exceed #{BALANCE_LIMIT}" if @balance + amount > BALANCE_LIMIT
     @balance += amount
   end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+
+    fail "Already touched in" if in_journey?
+
+    @in_journey = true
+    self
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_out
+
+    fail "Already touched out" unless in_journey?
+
+    @in_journey = false
+    self
+  end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,11 +9,42 @@ describe Oystercard do
   describe '#top_up(amount)' do
     it 'increases the balance by amount' do
       subject.top_up(10)
-      expect(subject.balance).to eq 10 
+      expect(subject.balance).to eq 10
     end
-    it 'throw an error if the new balance is above limit' do 
+    it 'throw an error if the new balance is above limit' do
       subject.top_up(Oystercard::BALANCE_LIMIT)
       expect { subject.top_up(1) }.to raise_error "Balance cannot exceed #{Oystercard::BALANCE_LIMIT}"
+    end
+  end
+
+  describe '#deduct(amount)' do
+    it 'deducts an amount from the balance' do
+      card = Oystercard.new(10)
+      card.deduct(5)
+      expect(card.balance).to eq(5)
+    end
+  end
+
+  describe '#touch_in' do
+    it 'Changes in_journey to true' do
+    subject.touch_in
+    expect(subject.in_journey?).to eq true
+    end
+
+    it 'Prevents touching in when in journey' do
+      subject.touch_in
+      expect { subject.touch_in }.to raise_error("Already touched in")
+    end
+  end
+
+  describe '#touch_out' do
+    it 'Changes in_journey to false' do
+      subject.touch_in.touch_out
+      expect(subject.in_journey?).to eq false
+    end
+
+    it 'Prevents touching out when not in journey' do
+      expect { subject.touch_out }.to raise_error("Already touched out")
     end
   end
 end
